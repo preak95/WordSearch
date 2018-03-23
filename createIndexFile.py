7f4afab6b7ddb80a3d7cd4cf44abf919c1be47e5
preak95 <ps109149@gmail.com>
2018-03-24 04:23:59 +0530

    The first version of search and recommend was added which is working well for a smaller number of file

--- createIndexFile.py
+++ createIndexFile.py
@@ -35,6 +35,7 @@ from tkFileDialog import askopenfilename
 import re
 import hashlib
 from pymongo import MongoClient
+import os
 Tk().withdraw()
 
 
@@ -42,8 +43,8 @@ Tk().withdraw()
 connection = MongoClient()
 db = connection.wordsearch
 
-
-textfilename = askopenfilename()  # Opens window to gt the name of the text file
+"""
+textfilename = askopenfilename()  # Opens window to get the name of the text file
 
 print(textfilename)  # Prints the exact location of the file
 
@@ -64,7 +65,7 @@ hash1 = hashlib.md5()
 hash1.update(filename)
 print("Hash: " + str(hash1.hexdigest()))
 
-
+"""
 class Worddictionary:
     """This will manage the list of all the dictionary entries"""
     def __init__(self):
@@ -86,15 +87,59 @@ class Worddictionary:
 # This is a different part where I will try and make use of hash
 # tables to deal with the problem
 
+def createindexfromabook(wordarray, filename):
+    wordHashTable = {}
+
+    pos = 1
+    for word in wordarray:
+        if word not in wordHashTable:
+            wordHashTable[word] = [pos]
+        else:
+            wordHashTable[word].append(pos)
+        pos += 1
+
+    wordDictionaryList = []
+    filename = re.compile("(.*/)*").split(filename.name)[-1]
+    for k in wordHashTable:
+        wordDictionaryList.append({
+            'word': k,
+            'positions': wordHashTable[k],
+            'fileName': str(filename)
+        })
+
+    for word in wordDictionaryList:
+        db.index.insert({
+            "word": word['word'],
+            "positions": word['positions'],
+            "fileName": word['fileName']
+        })
+
+def filechooser(dir):
+    for txtfile in os.listdir(dir): #iterate through pdfs in pdf directory
+        fileExtension = txtfile.split(".")[-1]
+        wordarray = []
+        if fileExtension == "txt":
+            textfile = open(dir + txtfile)
+            wordarray = re.compile('\w+').findall(textfile.read())
+
+            # Extract the name of the book
+            filename = re.compile("(.*/)*").split(textfile.name)[-1]
+
+            createindexfromabook(wordarray, textfile)
+            print "Added to index: " + filename
+
+filechooser("C:/Users/ps109/Desktop/Project/WordSearch/text/")
+
+"""
 wordHashTable = {}
 
-cn = 1
+pos = 1
 for word in wordarray:
     if word not in wordHashTable:
-        wordHashTable[word] = [cn]
+        wordHashTable[word] = [pos]
     else:
-        wordHashTable[word].append(cn)
-    cn += 1
+        wordHashTable[word].append(pos)
+    pos += 1
 
 wordDictionaryList = []
 for k in wordHashTable:
@@ -108,7 +153,7 @@ for k in wordHashTable:
 
 cnt = 1
 
-"""
+
 for word in wordarray:
     # print("Trn No.: " + str(cnt))
     wd.dictInsert(word, cnt)
@@ -118,7 +163,7 @@ cnt = 0
 for wd in wd.dictionaryList:
     if cnt < 20:
         print wd
-"""
+
 
 cn = 0
 for x in wordDictionaryList:
@@ -142,3 +187,4 @@ for word in wordarray:
         cn += 1
 
 print ("Machine: " + str(cn))
+"""
--- searchAlgorithm.py
+++ searchAlgorithm.py
@@ -1,6 +1,95 @@
 from pymongo import MongoClient
 
+"""
+Hello fellas.
+In this piece of code, we will develop and algorithm to extract desired data from the MongoDB database
+that we created earlier. Let us now see in detail what we are gonna do.
+
+23/03/18 This is the first version of the algorithm so I expect it to have a higher time complexity
+
+1. How was the structure of out DB?
+    We had a document named index that stored data in this format:
+        db.index.insert({
+            "word": word['word'],
+            "positions": word['positions'],
+            "fileName": word['fileName']
+        })
+        
+2. As an input we will have a series of "words" that we will need to search through the database
+    and accordingly find out what book these words belong to
+    
+3. To do so we begin with the first word.
+        i. We create a set of all the books that have the word w1 in them by using the following query on the DB
+            db.find({"word" : w1})
+            This will return all the objects containing the word w1(For a huge number of books, this number
+            will be very large)
+        ii. We then receive w2, we repeat the process and create a set of all the books with w2 in them.
+            This is then followed by a process that will create an intersection of all the books in the two sets.
+"""
+
+
 # Create a new connection to a single MongoDB instance at host:port.
 connection = MongoClient()
 db = connection.wordsearch
 
+w1 = "Although"
+w2 = "splendid"
+
+search1 = list(db.index.find({"word": w1}))
+search2 = list(db.index.find({"word": w2}))
+
+searchset1 = set()
+searchset2 = set()
+
+for entry in search1:
+    searchset1.add(entry["fileName"])
+
+for entry in search2:
+    searchset2.add(entry["fileName"])
+
+intersectionSet = searchset1.intersection(searchset2)
+
+worddifference = 80
+
+books = []
+
+for entry in intersectionSet:
+    books.append(entry)
+
+bookworddictionary = {}
+
+for book in books:
+    for entry in search1:
+        if entry["fileName"] == book and entry["word"] == w1:
+            bookworddictionary[(w1, book)] = entry["positions"]
+
+for book in books:
+    for entry in search2:
+        if entry["fileName"] == book and entry["word"] == w2:
+            bookworddictionary[(w2, book)] = entry["positions"]
+
+#print bookworddictionary
+
+# print bookworddictionary.keys()
+
+l1 = []
+l2 = []
+
+solutiontuple = []
+
+
+""" 
+What worries me is the complexity of the code snippet below.
+Suppose there are k books, l1 has m and l2 has n entries, then the complexity of the snipped would be 
+ O(knm)
+"""
+for book in books:
+    l1 = bookworddictionary[(w1, book)]
+    l2 = bookworddictionary[(w2, book)]
+
+    for i in l1:
+        for j in l2:
+            if worddifference - 5 < j - i < worddifference + 5:
+                solutiontuple.append((book, i, j))
+
+print solutiontuple
